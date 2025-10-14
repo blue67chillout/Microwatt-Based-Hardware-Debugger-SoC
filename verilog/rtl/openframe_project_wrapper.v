41fe58d2f4864aba7aa4da05f3720b25b5f0f408
blue67chillout <arteckcr.221ec224@nitk.edu.in>
2025-10-15 01:16:37 +0530

    added src

--- verilog/rtl/openframe_project_wrapper.v
+++ verilog/rtl/openframe_project_wrapper.v
@@ -103,24 +103,26 @@ module openframe_project_wrapper (
     input  [`OPENFRAME_IO_PADS-1:0] gpio_loopback_zero
 );
 
-	user_proj_timer mprj (
-`ifdef USE_POWER_PINS
-		.vccd1(vccd1),
-		.vssd1(vssd1),
-`endif
-        .wb_clk_i(gpio_in[0]),
-        .wb_rst_i(gpio_in[1]),
-        .io_in(gpio_in[12:2]),
-        .io_out(gpio_out[12:2]),
-        .io_oeb(gpio_oeb[12:2])
+// 	user_proj_timer mprj (
+// `ifdef USE_POWER_PINS
+// 		.vccd1(vccd1),
+// 		.vssd1(vssd1),
+// `endif
+//         .wb_clk_i(gpio_in[0]),
+//         .wb_rst_i(gpio_in[1]),
+//         .io_in(gpio_in[12:2]),
+//         .io_out(gpio_out[12:2]),
+//         .io_oeb(gpio_oeb[12:2])
 
-	    /* NOTE:  Openframe signals not used in picosoc:	*/
-	    /* porb_h:    3.3V domain signal			*/
-	    /* resetb_h:  3.3V domain signal			*/
-	    /* gpio_in_h: 3.3V domain signals			*/
-	    /* analog_io: analog signals			*/
-	    /* analog_noesd_io: analog signals			*/
-	);
+// 	    /* NOTE:  Openframe signals not used in picosoc:	*/
+// 	    /* porb_h:    3.3V domain signal			*/
+// 	    /* resetb_h:  3.3V domain signal			*/
+// 	    /* gpio_in_h: 3.3V domain signals			*/
+// 	    /* analog_io: analog signals			*/
+// 	    /* analog_noesd_io: analog signals			*/
+// 	);
+
+
 
 	/* All analog enable/select/polarity and holdover bits	*/
 	/* will not be handled in the picosoc module.  Tie	*/
